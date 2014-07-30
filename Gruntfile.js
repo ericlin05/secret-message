90741c6bad03db838af9a2c4a22d0053ffbc4c29
Eric Lin <eric.lin@effectivemeasure.com>
2014-07-30 14:28:41 +1000

    get grunt and bower working

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        "bower-install-simple": {
+            options: {
+                color:       true,
+                production:  false
+            }
+        },
+        concat: {
+            options : {
+                separator : ';',
+                stripBanners : { block : true }
+            },
+            js: {
+                options: {
+                    // define a string to put between each file in the concatenated output
+                    separator: "\n"
+                },
+                // the files to concatenate
+                src: [
+                    "bower_components/es5-shim/es5-shim.js",
+                    "bower_components/angular/angular.js",
+                    "bower_components/ng-file-upload/angular-file-upload.js",
+                    "bower_components/ng-file-upload/angular-file-upload-shim.js",
+                    "bower_components/angular-route/angular-route.js",
+                    "bower_components/ngDialog/js/ngDialog.js",
+                    "bower_components/jquery/jquery.js",
+                    "bower_components/bootstrap/dist/js/bootstrap.js",
+                    "src/js/controller.js"
+                ],
+                // the location of the resulting JS file
+                dest: 'public/js/<%= pkg.name %>.js'
+            },
+            css: {
+                options: {
+                    // define a string to put between each file in the concatenated output
+                    separator: "\n"
+                },
+                // the files to concatenate
+                src: [
+                    "bower_components/ngDialog/css/ngDialog.css",
+                    "bower_components/ngDialog/css/ngDialog-theme-default.css",
+                    "bower_components/bootstrap/dist/css/bootstrap.css",
+                    "bower_components/bootstrap/dist/css/bootstrap-theme.css",
+                    "src/css/jumbotron-narrow.css",
+                    "src/css/main.css"
+                ],
+                // the location of the resulting JS file
+                dest: 'dist/css/<%= pkg.name %>.css'
+            }
+        },
+        cssmin : {
+            options : {
+                report : 'gzip'
+            },
+            minify : {
+                expand : true,
+                cwd : 'dist/css',
+                src : ['<%= pkg.name %>.css'],
+                dest : 'public/css',
+                ext : '.min.css'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'dist/js/<%= pkg.name %>.js',
+                dest: 'public/js/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.loadNpmTasks('grunt-ender');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-bower-install-simple');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-wiredep');
+
+    // Default task(s).
+    grunt.registerTask('build', ['bower-install-simple', 'concat', 'cssmin']);
+
+};
+
